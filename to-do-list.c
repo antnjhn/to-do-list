f4d4cbcc27c4106ab093c56204a1552cc9a477c3
antnjhn <159994005+antnjhn@users.noreply.github.com>
2024-04-16 00:22:16 +0530

    Update to-do-list.c

--- to-do-list.c
+++ to-do-list.c
@@ -1,13 +1,27 @@
 #include<stdio.h>
 #include<string.h>
+void deletetask(const char *filename,int taskname)
+    {   
+        int line=1;
+        FILE *fp1,*fp2;
+        char buffer[100];
+        
+    }
 int main()
     {    
         FILE *fp;
-        int k,i,a,z=1,taskno;
+        int k,i,a,z=1,taskno=1;
         char buffer[100];
         char task[100];
         char opt;
-        do{
+        do{ 
+            printf("___________________    ________   ________    .____    .___  ____________________\n");
+        printf("\\__    ___/\\_____  \\   \\______ \\  \\_____  \\   |    |   |   |/   _____/\\__    ___/\n");
+        printf("  |    |    /   |   \\   |    |  \\  /   |   \\  |    |   |   |\\_____  \\   |    |\n");
+        printf("  |    |   /    |    \\  |    `   \\/    |    \\ |    |___|   |/        \\  |    |\n");
+        printf("  |____|   \\_______  / /_______  /\\_______  / |_______ \\___/_______  /  |____|\n");
+        printf("                   \\/          \\/         \\/          \\/           \\/\n\n");
+
             printf("(1)-Display\n(2)-New tasks\n");
             printf("Enter your choice??:");
             scanf("%d",&a);
@@ -15,12 +29,12 @@ int main()
             if (a == 2)
                 {
              //writing to the file
+                fp=fopen("to do list.txt","a");
                 if (fp == NULL)
                     {
                         printf("Error opening the file ");
                         return 1;
                     }
-                fp=fopen("to do list.txt","a");
                 printf("Enter how many tasks to add to the to do list?:");//to iterate  the no of times to be written in the file
                 scanf("%d",&k);
                 //consume the newline character left in the input buffer
@@ -35,7 +49,7 @@ int main()
                                 task[strlen(task) -1] = '\0';/*this line of code replaces the newline character(\n) from the end of the character with a null terminator*/
                             }
                         fprintf(fp,"%d:%s\n",taskno,task);
-                        taskno++;
+                        ++taskno;
                     }
                 fclose(fp);
                 }
@@ -61,7 +75,7 @@ int main()
                     printf("Check what you've written\n");
                 }
             printf("Do you want to continue?(Y/N):");
-            scanf("%c",&opt);
+            scanf(" %c",&opt);
             getchar();
             }while(opt =='Y' || opt == 'y');
             return 0;
